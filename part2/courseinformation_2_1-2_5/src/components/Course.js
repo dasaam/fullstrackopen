757bd35762ed60108fac8fbbc737ae0298172617
Daniel <na.da.007@outlook.com>
2022-10-17 11:14:33 -0500

    part2 course information 2.1 step 6

---
+++ part2/courseinformation_2_1-2_5/src/components/Course.js
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Content from './Content'
+
+const Course = props => {
+
+    return (
+        <>
+        <Header course={ props.course.name }/>
+        <Content parts={ props.course.parts }/>
+        
+        </>
+    )
+}
+
+export default Course
