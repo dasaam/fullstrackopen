bec94a319326a7441e2111651953608b93cabec5
Daniel <na.da.007@outlook.com>
2022-10-19 12:56:54 -0500

    part2 course information 2.4 step 9

--- part2/courseinformation_2_1-2_5/src/components/Course.js
+++ part2/courseinformation_2_1-2_5/src/components/Course.js
@@ -1,12 +1,25 @@
 import Header from './Header'
 import Content from './Content'
+import React from 'react'
 
 const Course = props => {
 
+
     return (
         <>
-        <Header course={ props.course.name }/>
-        <Content parts={ props.course.parts }/>
+        <h1>Web development curriculum</h1>
+        {
+            props.courses.map(
+                course => {
+                    return (
+                        <React.Fragment key={course.id}>
+                            <Header  course={ course.name }/> 
+                            <Content  parts={ course.parts }/>
+                        </React.Fragment>
+                    )
+                }
+            )
+        }
         </>
     )
 }
