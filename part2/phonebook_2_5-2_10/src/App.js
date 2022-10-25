a6361a981019efc6b6748cf1bdac4aa3994a5e3a
Daniel <na.da.007@outlook.com>
2022-10-25 10:55:51 -0500

    part2 phonebook 2.9 step 4

--- part2/phonebook_2_5-2_10/src/App.js
+++ part2/phonebook_2_5-2_10/src/App.js
@@ -3,10 +3,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number:'39-44-5323523' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -17,6 +22,17 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
+  const personsFilter = (filter == '') ? persons : persons.filter((person) => {
+    return person["name"].toLowerCase().indexOf(filter.toLowerCase()) !== -1
+  })
+
+  console.log(personsFilter)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -40,6 +56,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value = {filter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit = { addPerson }>
         <div>
           name: <input value = {newName} onChange={handleNameChange}/>
@@ -55,7 +75,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsFilter.map(person =>
           <Person key={person.name} person={person} />
         )}
       </ul>
