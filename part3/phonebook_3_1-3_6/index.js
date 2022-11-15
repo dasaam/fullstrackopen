fc768934f07414d889b4d6d698f9a1938848d1b0
Daniel <na.da.007@outlook.com>
2022-11-15 15:52:52 -0600

    part3´phonebook 3.5 step 5

--- part3/phonebook_3_1-3_6/index.js
+++ part3/phonebook_3_1-3_6/index.js
@@ -61,6 +61,31 @@ app.get('/api/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>`+ new Date())
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const generateId = () => {
+  const newRandomId = Math.floor(Math.random() * 10000)
+  return newRandomId
+}
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
