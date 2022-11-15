b5d61fa7166e461eab52d5e60d66405f1ec9dbc9
Daniel <na.da.007@outlook.com>
2022-11-15 15:33:10 -0600

    part3 phonebook 3.2 step 2

--- part3/phonebook_3_1-3_6/index.js
+++ part3/phonebook_3_1-3_6/index.js
@@ -36,6 +36,10 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>`+ new Date())
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
