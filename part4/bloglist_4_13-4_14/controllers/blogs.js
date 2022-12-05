e36d1aca4247f22a4ac802cd87c7bc99df30fa0b
Daniel <na.da.007@outlook.com>
2022-12-05 13:23:15 -0600

    part4 bloglist 4.14 step 2

--- part4/bloglist_4_13-4_14/controllers/blogs.js
+++ part4/bloglist_4_13-4_14/controllers/blogs.js
@@ -17,5 +17,17 @@ blogsRouter.delete('/:id', async(request, response) => {
     response.status(204).end()    
 })
 
+blogsRouter.put('/:id', async(request, response, next) => {
+    const body = request.body
+  
+    const blog = {
+      likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+})
+  
+
 module.exports = blogsRouter
   
