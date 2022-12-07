e2420b4f0d1ab1801595a2225cd662f09fc22348
Daniel <na.da.007@outlook.com>
2022-12-07 11:27:58 -0600

    part4 bloglist 4.15 step 3

---
+++ part4/bloglist_4_15-4_22/controllers/blogs.js
@@ -0,0 +1,33 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+})
+  
+blogsRouter.post('/', async(request, response) => {
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()    
+})
+
+blogsRouter.put('/:id', async(request, response, next) => {
+    const body = request.body
+  
+    const blog = {
+      likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+})
+  
+
+module.exports = blogsRouter
+  
