5d3493f10a6ac49ca214beacc31e9954ff6845a0
Daniel <na.da.007@outlook.com>
2023-01-09 15:24:54 -0600

    part5 frontend bloglist 5.7 step 7

--- part5/bloglist_frontent_5_5-5_12/src/components/Blog.js
+++ part5/bloglist_frontent_5_5-5_12/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>      
+      <div>
+        
+        <div style={hideWhenVisible}>
+        {blog.title} <button onClick={toggleVisibility}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+          <p>{blog.title}</p>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+      </div>  
+    </div>
+)}
+ 
 export default Blog
