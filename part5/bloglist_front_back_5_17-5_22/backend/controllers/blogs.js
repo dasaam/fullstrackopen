b7df7e276278706f68e8e3c6f7a87d4d7db07159
Daniel <na.da.007@outlook.com>
2023-01-16 13:58:19 -0600

    part5 bloglist 5.17 step 1

---
+++ part5/bloglist_front_back_5_17-5_22/backend/controllers/blogs.js
@@ -0,0 +1,76 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {  
+    const authorization = request.get('authorization')  
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {    
+      return authorization.substring(7)  
+    } 
+    return null
+}
+
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
+})
+  
+blogsRouter.post('/', async(request, response) => {
+    const body = request.body
+    const token = getTokenFrom(request)  
+    const decodedToken = jwt.verify(token, process.env.SECRET) 
+
+    if (!token || !decodedToken.id) {    
+        return response.status(401).json({ error: 'token missing or invalid' }) 
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+        title: body.title,
+        author: user.name,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+    
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
+    const token = getTokenFrom(request)  
+    const decodedToken = jwt.verify(token, process.env.SECRET) 
+
+    if (!token || !decodedToken.id) {    
+        return response.status(401).json({ error: 'token missing or invalid' }) 
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    if(blog.user.toString() === user.id.toString()){
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()    
+    }else{
+        return response.status(401).json({ error: 'the post that you try delete not belogs you' }) 
+    }
+})
+
+blogsRouter.put('/:id', async(request, response, next) => {
+    const body = request.body
+  
+    const blog = {
+      likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+})
+  
+
+module.exports = blogsRouter
+  
---
+++ part5/bloglist_front_back_5_17-5_22/frontend/src/components/BlogForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    createBlog({
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+      likes: 0
+    })
+
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addBlog}>
+        <p>
+          <label>title</label>
+          <input type="text" name="title" value={newTitle} onChange={({ target }) => setNewTitle(target.value)} />
+        </p>
+        <p>
+          <label>author</label>
+          <input type="text" name="author" value={newAuthor} onChange={({ target }) => setNewAuthor(target.value)} />
+        </p>
+        <p>
+          <label>url</label>
+          <input type="text" name="url" value={newUrl} onChange={({ target }) => setNewUrl(target.value)} />
+        </p>
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
