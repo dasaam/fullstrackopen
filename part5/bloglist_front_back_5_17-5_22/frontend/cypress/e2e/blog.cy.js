79f8e2a8fee1208b1db8fed76e0f8ce08f323430
Daniel <na.da.007@outlook.com>
2023-01-16 14:41:55 -0600

    part5 bloglist 5.18 step 2

--- part5/bloglist_front_back_5_17-5_22/frontend/cypress/e2e/blog.cy.js
+++ part5/bloglist_front_back_5_17-5_22/frontend/cypress/e2e/blog.cy.js
@@ -1,6 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {      
+      name: 'Superuser',      
+      username: 'root',      
+      password: 'salainen'    
+    }    
+    
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
 
@@ -10,5 +17,30 @@ describe('Blog app', function() {
     cy.get('#password')
     cy.get('#login-button')
     cy.contains('login')
+  })
+
+  describe.only('Login', function() {
+    
+    it('succeeds with correct credentials', function() {
+      
+      cy.get('#username').type('root')    
+      cy.get('#password').type('salainen')    
+      cy.get('#login-button').click()
+      
+      cy.contains('Superuser logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('root')    
+      cy.get('#password').type('corn')    
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+      .should('contain', 'Wrong username or password')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+  
+      cy.get('html').should('not.contain', 'Superuser logged in')
+    })
   })
 })
