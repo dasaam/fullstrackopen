b29bd02a8f5d904e76fc1647ee6f7eb6386b06bd
Daniel <na.da.007@outlook.com>
2023-01-19 14:59:17 -0600

    part6 unicafe-redux 6.1 step 1

---
+++ part6/unicafe-redux_6_1-6_2/src/reducer.js
@@ -0,0 +1,41 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const goodChanged = {
+        good: state.good + 1,
+        ok: state.ok,
+        bad: state.bad
+      }
+      
+      return goodChanged
+    case 'OK':
+      const okChanged = {
+        good: state.good,
+        ok: state.ok + 1,
+        bad: state.bad
+      }
+      
+      return okChanged
+    case 'BAD':
+      const badChanged = {
+        good: state.good,
+        ok: state.ok,
+        bad: state.bad + 1
+      }
+      
+      return badChanged
+    case 'ZERO':
+      return state
+    default: return state
+  }
+  
+}
+
+export default counterReducer
