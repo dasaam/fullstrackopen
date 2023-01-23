b5c7b7327ecae4893f53a01b1024b623aeaceae0
Daniel <na.da.007@outlook.com>
2023-01-23 11:56:24 -0600

    part6 anecdotes-redux 6.4 step 2

--- part6/anecdotes-redux_6_3-6_8/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux_6_3-6_8/src/reducers/anecdoteReducer.js
@@ -34,6 +34,8 @@ const reducer = (state = initialState, action) => {
       }
 
       return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote )
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     default:
       return state
   }
@@ -46,6 +48,17 @@ export const generateVote = (noteId) => {
     type: 'VOTE',
     data: {
       id: noteId
+    }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
     }
   }
 }
