cd52bdc67a0038cbe441b3d4b08fc138bf6c5ede
Daniel <na.da.007@outlook.com>
2023-01-31 09:40:57 -0600

    part6 anecdotes-redux 6.16 step 4

--- part6/anecdotes-redux_6_15-6_18/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux_6_15-6_18/src/components/AnecdoteForm.js
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { sendNotificationCreate} from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch()
@@ -11,8 +10,7 @@ const AnecdoteForm = (props) => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     
-    const newAnecdote = await anecdoteService.createNew(content)    
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
 
 
     dispatch(sendNotificationCreate(`you created ${content}`))
--- part6/anecdotes-redux_6_15-6_18/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux_6_15-6_18/src/reducers/anecdoteReducer.js
@@ -35,10 +35,13 @@ export const generateVote = (noteId) => {
   }
 }
 
-export const createAnecdote = (data) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote,
+    })
   }
 }
 
