cc0fcc64920f30dfca52ac3f43c53d63abe48791
Daniel <na.da.007@outlook.com>
2023-02-01 14:51:05 -0600

    part6 anecdotes-redux 6.20 step 2

--- part6/anecdotes-redux_6_19-6_20/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux_6_19-6_20/src/components/AnecdoteForm.js
@@ -1,22 +1,22 @@
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { sendNotificationCreate} from '../reducers/notificationReducer'
 
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch()
+  
 
   const addAnecdote = async(event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     
-    dispatch(createAnecdote(content))
+    props.createAnecdote(content)
 
 
-    dispatch(sendNotificationCreate(`you created ${content}`))
+    props.sendNotificationCreate(`you created ${content}`)
     setTimeout(() => {
-      dispatch(sendNotificationCreate(''))  
+      props.sendNotificationCreate('') 
     }, 5000)
   }
 
@@ -28,4 +28,13 @@ const AnecdoteForm = (props) => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  sendNotificationCreate
+}
+
+export default connect(  
+  null,   
+  mapDispatchToProps
+)(AnecdoteForm)
+
